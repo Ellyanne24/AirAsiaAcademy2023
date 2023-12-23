adeb2e99282f64a9c6915e78f081198ecdbebbd9
Ellyanne24 <154413656+Ellyanne24@users.noreply.github.com>
2023-12-23 09:10:54 +0800

    Update Iris_sp.py

--- Iris_sp.py
+++ Iris_sp.py
@@ -30,7 +30,7 @@ prediction = modelIrisClass.predict(df)
 prediction_proba = modelIrisClass.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(df.species.value_counts())
+st.write(df.species.unique())
 
 st.subheader('Prediction')
 st.write(prediction)
