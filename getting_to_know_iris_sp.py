3bc40e65046e2ef83820db40f4828e9d95086ca7
Ellyanne24 <154413656+Ellyanne24@users.noreply.github.com>
2023-12-23 09:22:10 +0800

    Update getting_to_know_iris_sp.py

--- getting_to_know_iris_sp.py
+++ getting_to_know_iris_sp.py
@@ -31,7 +31,7 @@ prediction = modelIrisClass.predict(df)
 prediction_proba = modelIrisClass.predict_proba(df)
 
 st.subheader('Species categories and their corresponding index number')
-st.write(Y.prediction())
+st.write(prediction())
 
 st.subheader('Prediction')
 st.write(prediction)
